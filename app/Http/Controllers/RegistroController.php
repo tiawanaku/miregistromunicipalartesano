3c5fea56ec020781628bb329a3dc79ebbed3a08d
alanezfranz <alanezfranz@gmail.com>
2023-11-19 17:37:56 -0400

    solve error del mensaje

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -9,7 +9,8 @@ class RegistroController extends Controller
 {
     public function insertar(Request $request)
     {
-        return Registro::create([
+        // Crear un nuevo registro en la base de datos con los datos del formulario
+        Registro::create([
             'nombre' => $request->input('nombre'),
             'apellido_paterno' => $request->input('apellido_paterno'),
             'apellido_materno' => $request->input('apellido_materno'),
@@ -26,18 +27,18 @@ class RegistroController extends Controller
             'asociacion' => $request->input('asociacion'),
             'producto_principal' => $request->input('producto_principal'),
             'producto_secundario' => $request->input('producto_secundario'),
-            'licencia_funcionamiento' => $request->has('licencia_funcionamiento') ? 1 : 0,
-            '$impuesto_venta' => $request->has('impuesto_venta') ? 1 : 0,
-            'impuesto_venta' => $request->has('impuesto_venta') ? 1 : 0,
-            'impuesto_productiva' => $request->has('impuesto_productiva') ? 1 : 0,
-            'impuesto_comercio' => $request->has('impuesto_comercio') ? 1 : 0,
-            'impuesto_feria' => $request->has('impuesto_feria') ? 1 : 0,
-            'impuesto_ninguno' => $request->has('impuesto_ninguno') ? 1 : 0,
-            'impuesto_otro' => $request->has('impuesto_otro') ? 1 : 0
+            'licencia_funcionamiento' => $request->has('licencia_funcionamiento'),
+            'impuesto_venta' => $request->has('impuesto_venta'),
+            'impuesto_productiva' => $request->has('impuesto_productiva'),
+            'impuesto_comercio' => $request->has('impuesto_comercio'),
+            'impuesto_feria' => $request->has('impuesto_feria'),
+            'impuesto_ninguno' => $request->has('impuesto_ninguno'),
+            'impuesto_otro' => $request->has('impuesto_otro')
         ]);
 
+        // Devolver una respuesta simple
         return response('Registro creado con éxito', 200);
-
     }
 }
+
 
